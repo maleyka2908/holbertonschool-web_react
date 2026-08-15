dff99edf850d68deaea989dfb12d5f994ec5a66e
maleyka2908 <13584@holbertonstudents.com>
2026-08-15 08:02:46 +0000

    Task 7 completed - String literal type Subjects and teachClass added

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -56,5 +56,14 @@ export function executeWork(employee: Director | Teacher): string {
   return employee.workTeacherTasks();
 }
 
-console.log(executeWork(createEmployee(200)));
-console.log(executeWork(createEmployee(1000)));
+export type Subjects = 'Math' | 'History';
+
+export function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  }
+  return 'Teaching History';
+}
+
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
